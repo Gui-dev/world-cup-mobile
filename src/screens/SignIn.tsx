c5078a1ece5cbeb157fdaf77ef2037926ae9a04a
Gui-dev <adrianon10@gmail.com>
2022-11-07 04:28:00 -0300

    feat: add screen SignIn

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,32 @@
+import { Center, Icon, Text } from 'native-base'
+import { Fontisto } from '@expo/vector-icons'
+
+import { Button } from '../components/Button'
+import Logo from './../assets/logo.svg'
+
+export const SignIn = () => {
+  return (
+    <Center
+      flex={1}
+      p={7}
+      bg="gray.900"
+    >
+      <Logo height={40} width={212} />
+      <Button
+        title="ENTRAR COM O GOOGLE"
+        type="SECONDARY"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        isLoading={false}
+        mt={12}
+      />
+      <Text
+        textAlign="center"
+        color="white"
+        mt={4}
+      >
+        Não utilizamos nenhuma informação além{'\n'}
+        do seu e-mail para criação de sua conta.
+      </Text>
+    </Center>
+  )
+}
