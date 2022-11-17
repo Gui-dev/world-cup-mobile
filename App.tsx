4f7193f74c8f16fd45508c3ddcd1b99cffb756fe
Gui-dev <adrianon10@gmail.com>
2022-11-16 21:13:18 -0300

    feat: add routes

--- App.tsx
+++ App.tsx
@@ -1,8 +1,11 @@
 import { StatusBar } from 'expo-status-bar'
-import { Box, NativeBaseProvider } from 'native-base'
+import { NativeBaseProvider } from 'native-base'
 import { Roboto_400Regular, Roboto_500Medium, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto'
 
 import { THEME } from './src/utils/theme'
+import { AuthContextProvider } from './src/contexts/AuthContext'
+import { Loading } from './src/components/Loading'
+import { Routes } from './src/routes'
 
 export default function App () {
   const [fontsLoaded] = useFonts({
@@ -12,13 +15,19 @@ export default function App () {
   })
 
   if (!fontsLoaded) {
-    return null
+    return (
+      <NativeBaseProvider theme={THEME}>
+        <Loading />
+      </NativeBaseProvider>
+    )
   }
 
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar style="light" backgroundColor="transparent" translucent />
-      <Box>Hello world</Box>
+      <AuthContextProvider>
+        <Routes />
+      </AuthContextProvider>
     </NativeBaseProvider>
   )
 }
